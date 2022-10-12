74d6f573d7e9220a1220c3d77a777d8f04fcd08c
Qvelent <qvelent@gmail.com>
2022-10-12 21:21:58 +0300

    Add move and rotate player

--- Assets/PlayerOption/Scripts/Player/Player.cs
+++ Assets/PlayerOption/Scripts/Player/Player.cs
@@ -4,16 +4,52 @@ namespace PlayerOption.Scripts.Player
 {
     public class Player : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private float speed;
+        [SerializeField] private float speedRotate = 100f;
+        [SerializeField] private float acceleration = 5f;
         
+        public float speedFMax = 30f;
+        
+        private float _rotate;
+        
+        private bool _speeding;
+        
+        private void FixedUpdate()
+        {
+            UpdateSpeed();
+            MoveRotate();
+        }
+        
+        private void UpdateSpeed()
+        {
+            if (_speeding)
+            {
+                if (speed < speedFMax)
+                {
+                    speed += acceleration * Time.deltaTime;
+                }
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+            else
+            {
+                if (speed > 0)
+                {
+                    speed -= acceleration * Time.deltaTime;
+                }
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+        
+        public void ChangeAcceleration(bool accelerationState)
+        {
+            _speeding = accelerationState;
         }
 
-        // Update is called once per frame
-        void Update()
+
+        public void MoveRotate()
         {
-        
+            _rotate = Input.GetAxis("Horizontal");
+            transform.Rotate(0,0,-_rotate * speedRotate * Time.deltaTime);
         }
     }
 }
--- Assets/PlayerOption/Scripts/Player/PlayerInput.cs
+++ Assets/PlayerOption/Scripts/Player/PlayerInput.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace PlayerOption.Scripts.Player
 {
     public class PlayerInput : MonoBehaviour
     {
         [SerializeField] private Player player;
+        
+        public void OnMovementForwardInput(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                player.ChangeAcceleration(true);
+            }
+
+            if (context.canceled)
+            {
+                player.ChangeAcceleration(false);
+            }
+        }
+
+        public void OnRotateLeftAndRightInput(InputAction.CallbackContext context)
+        {
+            player.MoveRotate();
+        }
     }
 }
